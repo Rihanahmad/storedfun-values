33b63f96058885352abcdb776cfe896864bd1fe2
RIHAN AHMAD <101428781+Rihanahmad@users.noreply.github.com>
2022-05-20 20:00:52 +0530

    Update stored fun values.sql

--- stored fun values.sql
+++ stored fun values.sql
@@ -1,6 +1,6 @@
-use rishab
+use rihan
 create table emp(id int,name varchar(50),salary money)
-insert into emp values(1,'rishab',1000000),(2,'6ty7uh5',299999),(3,'asdhj',83493409),(4,'asdgyh',215628),(5,'asgfd',68478)
+insert into emp values(1,'rihan',1000000),(2,'6ty7uh5',299999),(3,'asdhj',83493409),(4,'asdgyh',215628),(5,'asgfd',68478)
 select*from emp
 create function emp1(@id int)
 returns money
